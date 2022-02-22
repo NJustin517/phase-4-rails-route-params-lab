5c936a1f6b73e1a32145318da547d0e081636793
Justin Luciano <justinlluciano@gmail.com>
2022-02-22 08:58:49 -0800

    commit tree ba6b6083f3ea88d9327860d0654139a6272c3868

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,19 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    if (params[:name])
+      name = params[:name]
+      students = Student.where(["last_name = ?", name.capitalize] || ["first_name = ?", name.capitalize])
+      render json: students
+    else
+      students = Student.all
+      render json:students
+    end
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
